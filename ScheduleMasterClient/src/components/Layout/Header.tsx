af86313200411a5f454005299573dac7bedc7744
Nomi Magnus <NomiM@1221.org.il>
2025-05-06 11:34:20 +0300

    Delete unuse code

--- ScheduleMasterClient/src/components/Layout/Header.tsx
+++ ScheduleMasterClient/src/components/Layout/Header.tsx
@@ -34,7 +34,6 @@ export default function Header() {
                 setActiveIndex(e.index);
                 menuItems[e.index].command?.();
             }}
-            className="menu"
         />
     );
 
